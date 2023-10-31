694c89f40d794819e2d63e269602bb661ca1266a
Avumile <Smithavery435@gmail.com>
2023-10-31 07:29:41 -0700

    using string formatter to print numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    print(f"{num:02d}", end=", " if num < 99 else "\n")
-
+    if num < 99:
+        print("{:02d}, ".format(num), end="")
+    else:
+        print("{:02d}".format(num))
