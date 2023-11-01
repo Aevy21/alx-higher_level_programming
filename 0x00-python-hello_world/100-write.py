55b0ce657269587a7d0e9f7a02cbaaed7f3cefe7
Avumile <Smithavery435@gmail.com>
2023-11-01 03:40:50 -0700

    printing string using python

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -5,4 +5,5 @@ def main():
     message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
     sys.stderr.write(message + '\n')
     sys.exit(1)
-
+if __name__ == "__main__":
+    main()
