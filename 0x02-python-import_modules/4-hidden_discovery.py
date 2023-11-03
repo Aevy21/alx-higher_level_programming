1dbe87091f883040cfb58dd20bf93511f3791e80
Avumile <Smithavery435@gmail.com>
2023-11-02 18:45:51 -0700

    finding hiddenfiles and printing

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 
-import hidden_4
-
 if __name__ == "__main__":
-    # Get all attributes (names) from the module hidden_4
-    attributes = dir(hidden_4)
+    import hidden_4
 
-    # Iterate over attributes & print non-dunder
-    # / (not starting with '__') names
+    names = dir(hidden_4)
 
-    for attribute in sorted(attributes):
-        if not attribute.startswith("__"):
-            print(attribute)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
