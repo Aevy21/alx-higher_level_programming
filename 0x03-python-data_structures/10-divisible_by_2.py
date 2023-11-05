de5ea9594fa95732e51b8fdd5202dc4e81b7d36f
Avumile <Smithavery435@gmail.com>
2023-11-05 02:04:30 -0800

    finding intergers that are divisible by 2 in a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    result = []  # Create an empty list to store True or False values
+
+    for num in my_list:
+        if num % 2 == 0:
+            result.append(True)  # If the integer is a multiple of 2, append True
+        else:
+            result.append(False)  # If not, append False
+
+    return result
