b26c711e8df9f3ab2073d5c2a5c904b5e3fcd4d3
Avumile <Smithavery435@gmail.com>
2023-11-05 02:18:55 -0800

    deleting item at a givrn index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list  # If idx is negative or out of range, return the same list
+
+    del my_list[idx]
+    return my_list
