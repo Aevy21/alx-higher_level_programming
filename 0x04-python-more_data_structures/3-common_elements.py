1e95d9488b0979b4b674fb881e9085b0b88d6d4f
Avumile <Smithavery435@gmail.com>
2023-11-08 02:31:58 -0800

    using sets method to find common method

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    common_set = set()
+    # Create an empty set to store common elements
+
+    for item in set_1:
+        if item in set_2:
+            common_set.add(item)
+
+    return common_set
