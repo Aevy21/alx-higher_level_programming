98c27613d93b28551c7261e41bcf0305e62e1385
Avumile <Smithavery435@gmail.com>
2023-11-08 01:10:54 -0800

    matrix  math

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+def square_matrix_simple(matrix=[]):
+    # Create an empty result matrix with the same dimensions as the input matrix
+    new_matrix = []
+    for row in matrix:
+        result_row = []
+        # Initialize an empty row for the result matrix
+        for element in row:
+            # Calculate the square of the element
+            squared_element = element ** 2
+            result_row.append(squared_element)
+            # Append the squared element to the result row
+        new_matrix.append(result_row)  # Append the result row to the result matrix
+    return new_matrix  # Return the result matrix
+
