425665be04780056e5ec5ac251872a1a7627c6e9
Avumile <Smithavery435@gmail.com>
2023-11-08 03:56:46 -0800

    counting keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    count = 0  # Initialize the count
+
+    for key in a_dictionary:
+        count += 1  # Increment the count for each key
+
+    return count
