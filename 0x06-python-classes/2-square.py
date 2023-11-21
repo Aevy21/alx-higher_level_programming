7cf0b79e6d30ed0bf9464a8b3ed90233213d770a
Avumile <smithavery435@gmail.com>
2023-11-21 13:50:01 -0800

    errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,8 +11,8 @@ class Square:
     This class defines a square.
     """
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            if not isinstance(size, int):
+                raise TypeError("Size must be an integer")
 
             if size < 0:
                 raise ValueError("Size must be >= 0")
