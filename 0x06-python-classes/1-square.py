a9af74414970e29c1f2b55d18fa33ace3bf63e41
Avumile <smithavery435@gmail.com>
2023-11-21 07:01:18 -0800

    creating a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Note:
+        This class is a basic representation of a square \
+                and does not handle specific functionalities.
+    """
+
+
+class Square:
+    """
+    This class defines a square.
+    """
+    def __init__(self, size):
+        self.__size = size
