a33a867a3138fb3c31e380b7ff1d08fff89f8c7a
Avumile <smithavery435@gmail.com>
2023-11-21 09:03:59 -0800

    combining vlasses with exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Note:
+        This class is a basic representation of a square \
+                and does not handle specific functionalities.
+    """
+
+
+class Square:
+    """
+    This class defines a square.
+    """
+    def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        
+        self.__size = size
