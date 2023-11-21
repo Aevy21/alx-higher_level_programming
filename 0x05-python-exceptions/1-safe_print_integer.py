8ef512c6bff8aba022d1fd41cbc5d7801e378a5e
Avumile <smithavery435@gmail.com>
2023-11-20 18:01:24 -0800

    fixing value error message

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
         return True
-    except ValueError as e:
-        error_message = str(e)
-        return False, error_message
+    except ValueError :
+        return False
