64464d0148783d3c6ae17718210129ebb2c91cf4
Avumile <smithavery435@gmail.com>
2023-11-24 12:55:09 -0800

    errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 """
 Note: This function only adds integers and typecasted floats.
+Returns:
+    :return: The sum o a and b ,casted to an integer
 """
 
 def add_integer(a, b=98):
     """
-    Adds two numbers and returns the result as an integer.
-    
-    Parameters:
-    :param a: First number (integer or float).
-    :param b: Second number (integer or float, default is 98).
-    
-    Returns:
-    :return: The sum of a and b, casted to an integer.
+    parameter a: First number (integer or float).\
+            param b: Second number (integer or float, default is 98). 
     """
     
     # Check if both a and b are either integers or floats
