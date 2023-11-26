b03a413b9eaed592191bef4f7a1065ff60d5aa9c
Avumile <smithavery435@gmail.com>
2023-11-26 15:00:15 -0800

    indentatiin function using delims

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Note: function with a docstring added to provide a brief description of its purpose and usage
+"""
+
+def text_indentation(text):
+    """
+    Print text with two new lines after each '.', '?', and ':'
+
+    Parameters:
+    - text (str): The input text to be processed.
+
+    Raises:
+    - TypeError: If the input text is not a string.
+
+    Example Usage:
+    text = "This is a sample text. It has some questions: What is it about? Let's find out!"
+    text_indentation(text)
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    punctuation_chars = ['.', '?', ':']
+    lines = []            # Empty list to store processed lines
+    current_line = ''      # Empty string to accumulate characters until a punctuation is encountered
+
+    for char in text:
+        current_line += char
+        if char in punctuation_chars:
+            lines.append(' '.join(line.strip() for line in current_line.split(char)))
+            current_line = ''
+
+    if current_line:
+        lines.append(' '.join(line.strip() for line in current_line.split(char)))
+
+    print('\n\n'.join(lines))
+
