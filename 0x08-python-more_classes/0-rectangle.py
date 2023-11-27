defc950cdd449edde1612c97c6676bd06857b89a
Avumile <smithavery435@gmail.com>
2023-11-27 06:50:33 -0800

    starting up

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Note: This is an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    Rectangle Class:
+
+    This class defines a basic rectangle.
+    It currently has no attributes or methods.
+    """
+
+    pass  # The pass statement is a placeholder to create an empty class
