6e5af45d651b998efad9fb13c6c7ff149048b524
Avumile <smithavery435@gmail.com>
2023-11-27 05:36:57 -0800

    stye err

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,22 +18,23 @@ def matrix_divided(matrix, div):
     """
 
     # Check if matrix is a list of lists of integers or float
-    if not  all(isinstance(row, list) for row in matrix):
-        raise TypeError\
-                ("matrix must be a matrix (list of lists) of integers/floats")
-    
+    if not all(isinstance(row, list)for row in matrix):
+        raise TypeError("matrix must be a \
+                matrix (list of lists) of integers/floats")
+
     # Check if each element in the lists is an integer or float
     if not all(isinstance(num, (int, float)) for row in matrix for num in row):
-        raise TypeError\
-                ("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a \
+                matrix (list of lists) of integers/floats")
+
     if not matrix or not all(isinstance(row, list) and row for row in matrix):
-         raise TypeError\
-                ("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be\
+                a matrix (list of lists) of integers/floats")
 
     # Check if each row has the same size
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    
+
     # Check if div is a number (integer or float)
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
