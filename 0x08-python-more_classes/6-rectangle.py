aa56d620efba38958240af2e936ae072a8835535
Avumile <smithavery435@gmail.com>
2023-11-27 14:17:05 -0800

    errors

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
     - _width (int): Width of the rectangle.
     - _height (int): Height of the rectangle.
     """
-
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         """
         Initialize a Rectangle instance.
@@ -28,6 +28,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
