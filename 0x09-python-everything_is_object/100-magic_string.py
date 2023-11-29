e06024d4e4c5c232a4cdf606f56e90ee7f5ab5b1
Avumile <smithavery435@gmail.com>
2023-11-29 06:41:38 -0800

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,26 +1,3 @@
 #!/usr/bin/python3
-"""
-Note: This  function magic_string() that returns a \
-        string “BestSchool” n times the number of the iteration
-
-"""
-
-
 def magic_string(n):
-    """
-    Generate a string by concatenating "BestSchool" repeated \
-            n times based on iteration number.
-
-    Args:
-        n (int): Number of iterations.
-
-    Returns:
-        str: The generated string.
-    """
-    result = ""
-    for i in range(1, n + 1):
-        part = "BestSchool" * i
-        # Create a substring "BestSchool" repeated i times
-        result += part
-        # Append the substring to the result string
-    return result
+    return ''.join('BestSchool'*i for i in range(1, n+1))
