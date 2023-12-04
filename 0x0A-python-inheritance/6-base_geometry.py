1cf8462bdc7f18d0ca2929ba158a74452bfeda9e
Avumile <smithavery435@gmail.com>
2023-12-04 10:09:58 -0800

    defin*ng class with method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+definition of a class named BaseGeometry with an area method
+"""
+
+
+class BaseGeometry:
+    """A class with an area method."""
+
+    def area(self):
+        """Raises an Exception with msg"""
+        raise Exception("area() is not implemented")
