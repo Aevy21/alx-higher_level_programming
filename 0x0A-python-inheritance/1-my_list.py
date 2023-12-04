55a0cdee49335dfd590a7cba94c59a976207efe3
Avumile <smithavery435@gmail.com>
2023-12-04 08:48:17 -0800

    class inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """A custom list class inheriting from the built-in list class."""
+
+    def print_sorted(self):
+        """Prints the list in ascending sorted order."""
+        sorted_list = sorted(self)
+        print(sorted_list)
