31b0a42c9195b5d37f5e9960b40795c7f28370b2
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2023-12-04 16:12:05 +0200

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,9 +6,4 @@ A function that list all  of available attributes and methods of an object
 
 def lookup(obj):
     """Returns a list of attributes and methods of an object."""
-
-    result = []
-    for attribute_name in dir(obj):
-        if not callable(getattr(obj, attribute_name)):
-            result.append(attribute_name)
-    return result
+    return [attribute_name for attribute_name in dir(obj)]
