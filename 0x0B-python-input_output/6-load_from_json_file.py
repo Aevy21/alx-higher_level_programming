7d5ecf2edb5c20b3e0a735030317b22a7635bad4
Avumile <smithavery435@gmail.com>
2023-12-05 17:40:21 +0200

    creating an object file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+A function to create a Python object from a JSON file.
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates a Python object from a JSON file.
+
+    Parameters:
+    - filename (str): The name of the JSON file to load.
+
+    Returns:
+    object: The Python object created from the JSON file.
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return json.load(file)
