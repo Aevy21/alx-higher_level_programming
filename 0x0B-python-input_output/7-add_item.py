35193a0f583a98a257a6da72462e900d71b87ac7
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2023-12-06 01:39:47 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,20 +7,21 @@ import json
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
+
+    load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
+    # load the data from the 'add_item.json' file if it exists
     try:
-        # Try loading data from the 'add_item.json' file
-        loaded_data = load_from_json_file("add_item.json")
+        add_item_data = load_from_json_file("add_item.json")
 
-        # Concatenate the loaded data with the command-line arguments
+        # concatenating the loaded data with the command-line arguments
         # excluding the script name
-        loaded_data += argv[1:]
+        loaded_data = add_item_data + argv[1:]
 
     # handle the case where the JSON file does not exist or is empty
     except (FileNotFoundError, json.JSONDecodeError):
+        add_item_data = []
         loaded_data = argv[1:]
 
-    # Save the combined data back to the 'add_item.json' file
     save_to_json_file(loaded_data, "add_item.json")
