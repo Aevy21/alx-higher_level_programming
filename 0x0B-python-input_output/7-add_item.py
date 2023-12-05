7f00ce843493d152555a167f7fcff77e1ba4bc28
Avumile <smithavery435@gmail.com>
2023-12-06 00:19:25 +0200

    dealing wif command line args

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that adds command line args to a Python[] and saves it to a JSON file.
+"""
+
+import sys
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    # Load existing list from the file or create an empty list
+    myList = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    myList = []
+
+# Add command line arguments to the list
+myList.extend(sys.argv[1:])
+
+# Save the updated list to the file
+save_to_json_file(myList, "add_item.json")
