f97a41d1d4e80e69cabfebba07a693661bb67c07
Avumile <smithavery435@gmail.com>
2023-12-06 00:39:58 +0200

    doble files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+
 """
-script that adds command line args to a Python[] and saves it to a JSON file.
+Script that adds command line arguments to a Python list and saves it to a JSON file.
 """
 
 import sys
-import json
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
 # Load existing list from the file or create an empty list
 myList = load_from_json_file("add_item.json")
