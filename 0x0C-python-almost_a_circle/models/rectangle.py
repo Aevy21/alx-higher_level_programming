e8b5410f597cc9a0c96b03258ef3fb401eb1260f
Avumile <smithavery435@gmail.com>
2023-12-09 17:27:32 +0200

    an improvement on the public method def display(self):

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,3 +95,13 @@ class Rectangle(Base):
         """
         coords_str = "{}/{}".format(self.__x, self.__y)
         return "[Rectangle] ({}) {} - {}/{}".format(self.id, coords_str, self.__width, self.__height)
+
+    def display(self):
+        """
+        Prints the Rectangle instance with '#' characters, considering x and y.
+        """
+        for _ in range(self.__y):
+            print()
+
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
