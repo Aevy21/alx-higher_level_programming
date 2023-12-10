48d5c8313e4e79a4708531b241af2c607dfe180b
Avumile <smithavery435@gmail.com>
2023-12-10 15:32:39 +0200

    kwargs usage for the square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,29 +17,43 @@ class Square(Rectangle):
 
         Args:
             size (int): Size of the square.
-            x (int, optional): X-coordinate of the square. Defaults to 0.
-            y (int, optional): Y-coordinate of the square oherwise y=0.
-            id (int, optional): If provided, assigns the given ID, or d=None.
+            x (int, optional): X-coordinate of the square, else x=0.
+            y (int, optional): Y-coordinate of the square , else y=0.
+            id (int, optional): If provided, assigns the given ID or id=None.
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """
-        Getter for the size attribute.
-        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """
-        Setter for the size attribute.
-
-        Args:
-            value (int): New size value.
-        """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns attr to the Square instance using a list.
+
+        Args:
+            *args: List of arguments (id, size, x, y).
+            **kwargs: Dictionary of keyworded arguments.
+
+        Note:
+            **kwargs must be skipped if *args exists and is not empty.
+        """
+        attr_names = ["id", "size", "x", "y"]
+        if args:
+            attrs = args
+        elif kwargs:
+            attrs = [kwargs.get(attr, getattr(self, attr)) for attr in attr_names]
+        else:
+            return
+
+        for i, attribute in enumerate(attribute_names):
+            if i < len(attributes):
+                setattr(self, attribute, attributes[i])
 
     def __str__(self):
         """
