622e02f5b6aaf1e1bed6a9e00c300e60df819a97
Avumile <smithavery435@gmail.com>
2023-12-10 09:19:00 +0200

    pycodestyle fixing

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,29 +1,31 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """
-a class Base with a private class attribute __nb_objects and a constructor that either assigns the provided id \
-        or increments the class attribute to generate a new value for the public instance attribute id.
+a Base with a private class attr
 """
+
 
 class Base:
     """
-    
+
     Attributes:
-        __nb_objects (int): Private class attribute to keep track of the number of objects.
-        id (int): Public instance attribute representing the object's ID.
+    __nb_objects (int): Private class attr to keep track of the num of objects.
+    id (int): Public instance attr representing the object's ID.
     """
 
-    __nb_objects = 0  # private class attribute
+    __nb_objects = 0  # private class attr
 
     def __init__(self, id=None):
         """
         Args:
-            id (int, optional): If provided, assigns the given ID. If None, auto-increments the ID.
+            id (int, optional): If provided, assigns the given ID. \
+                    If None, auto-increments the ID.
 
         Note:
-            If id is not provided, it will be automatically generated using the __nb_objects counter.
+            If id is not provided, it will be auto generated\
+                    using the __nb_objects counter.
         """
         if id is not None:
             self.id = id  # assign id if provided
         else:
-            Base.__nb_objects += 1  # increment class attribute
+            Base.__nb_objects += 1  # increment class attr
             self.id = Base.__nb_objects  # assign the new value to id
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,9 @@
    Creating a Rectangle class, inherits from Base.
 """
 
+
 from models.base import Base
+
 
 class Rectangle(Base):
     """
@@ -15,6 +17,7 @@ class Rectangle(Base):
         __x (int): Private x attribute.
         __y (int): Private y attribute.
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Initializes a new instance of the Rectangle class.
@@ -23,9 +26,10 @@ class Rectangle(Base):
             width (int): Width of the rectangle.
             height (int): Height of the rectangle.
             x (int, optional): X-coordinate of the rectangle. Defaults to 0.
-            y (int, optional): Y-coordinate of the rectangle. Defaults to 0.
-            id (int, optional): If provided, assigns the given ID. Defaults to None.
+            y (int, optional): Y-coordinate of the rectangle or y=0.
+            id (int, optional): If provided, assigns the given ID.or id=None.
         """
+
         super().__init__(id)
         self.width = width
         self.height = height
@@ -59,7 +63,7 @@ class Rectangle(Base):
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         if not isinstance(value, int):
@@ -92,9 +96,13 @@ class Rectangle(Base):
     def __str__(self):
         """
         Returns a string representation of the Rectangle instance.
+        cod_str variable for coodinates
+        dim_str variable for dimensions attrubutes
         """
-        coords_str = "{}/{}".format(self.__x, self.__y)
-        return "[Rectangle] ({}) {} - {}/{}".format(self.id, coords_str, self.__width, self.__height)
+        cod_str = "{}/{}".format(self.__x, self.__y)
+        dim_str = "{}/{}".format(self.__width, self.__height)
+        rect_str = "[Rectangle] ({}) {} - {}".format(self.id, cod_str, dim_str)
+        return rect_str
 
     def display(self):
         """
@@ -115,4 +123,3 @@ class Rectangle(Base):
 
         for key, value in kwargs.items():
             setattr(self, key, value)
-
