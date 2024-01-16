a6f09430e5d981833376754d276d29d1e6e2480b
Avumile <Smithavery435@gmail.com>
2024-01-16 07:58:42 -0800

    try at the dvanc€d tasks

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Use the hbtn_0c_0 database
+USE `hbtn_0c_0`;
+
+-- Convert first_table in hbtn_0c_0 database to UTF8
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
