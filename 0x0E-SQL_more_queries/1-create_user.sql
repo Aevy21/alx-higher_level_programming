f4fec0af6688dbef58018c220f246d64d1364d6a
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-17 14:55:16 +0200

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Creating user_0d_1 with specified privileges if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
