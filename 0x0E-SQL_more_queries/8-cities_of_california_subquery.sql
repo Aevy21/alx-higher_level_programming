6fed0e4e21862ac39743bbc8fc057332ddd21eb1
Avumile <Smithavery435@gmail.com>
2024-01-17 08:57:00 -0800

    more on queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,5 @@
--- Replace 'your_database_name' with the actual database name passed as an argument
-USE hbtn_0d_usa;
-
--- Find the state_id for California in the states table
-SET @california_id := (SELECT id FROM states WHERE name = 'California');
-
--- List all cities of California sorted by cities.id
+-- Selecting cities of California without using JOIN
 SELECT cities.id, cities.name
-FROM cities
-WHERE cities.state_id = @california_id
+FROM cities, states
+WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id ASC;
