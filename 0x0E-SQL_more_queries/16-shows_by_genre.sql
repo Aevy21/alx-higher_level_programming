66a4632a151cf5a2d1f919fc6d2ce2493051ca8a
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-17 20:24:57 +0200

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Selecting shows and their linked genres (or NULL if no genre)
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre_name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
