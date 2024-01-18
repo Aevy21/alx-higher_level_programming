2b6c916636e4500bbeee2caaf27292a0f896f268
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-18 12:37:52 +0200

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,10 @@
+-- Selecting cities in California from the hbtn_0d_usa database
 SELECT cities.id, cities.name
 FROM cities
+-- Subquery to get the state_id of 'California' from the states table
 WHERE cities.state_id IN
-    ( SELECT states.id
-      FROM states
-      WHERE states.name = 'California' 
-    )
+    (SELECT states.id
+     FROM states
+     WHERE states.name = 'California')
+-- Ordering the results by cities.id
 ORDER BY cities.id;
