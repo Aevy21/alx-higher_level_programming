d2a3bc327e54179fb2938ddb6ff876d14410abf0
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-18 12:35:53 +0200

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
--- Selecting cities of California without using JOIN
-SELECT id, name
-  FROM cities
- WHERE state_id IN
-       (SELECT id
-	  FROM states
-	 WHERE name = "California")
- ORDER BY id;
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id IN
+    ( SELECT states.id
+      FROM states
+      WHERE states.name = 'California' 
+    )
+ORDER BY cities.id;
