b57e6efab205bfd4542b2e1c7f86bdb518cc8676
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-12 16:20:25 +0200

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const firstArgument = process.argv[2];
 const integerNumber = parseInt(firstArgument, 10);
 
 if (!isNaN(integerNumber)) {
-  console.log('My number: {integerNumber}');
+  console.log('My number: ' + integerNumber);
 } else {
   console.log('Not a number');
 }
