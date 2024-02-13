5b284ff345caeded90e10973268c5364efc2969f
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-13 23:20:30 +0200

    Update 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-exports.converter = function (base) {
-  return function convert (number) {
-    if (number < base) {
-      return '' + number;
-    }
-    return convert(Math.floor(number / base)) + (number % base);
-  };
+exports.converter = function(base) {
+    return function convert(number) {
+        if (number < base) {
+            return '' + (number < 10 ? number : String.fromCharCode(87 + number));
+        }
+        return convert(Math.floor(number / base)) + (number % base < 10 ? number % base : String.fromCharCode(87 + number % base));
+    };
 };
