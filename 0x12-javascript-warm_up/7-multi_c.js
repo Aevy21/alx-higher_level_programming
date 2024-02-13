9d0d9390fad797557e4fc7d327e20c2b2df6f2c3
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-13 10:45:00 +0200

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-
 const arg = process.argv[2];
 const x = parseInt(arg, 10);
 let count = 0;
 if (!isNaN(x)) {
-  
+
   while (count < x) {
     console.log('C is fun');
     count++;
