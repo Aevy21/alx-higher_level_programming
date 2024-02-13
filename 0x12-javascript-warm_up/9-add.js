e3900ec0ee72eeaeb066808692fc4f08444b7ee4
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-13 10:48:27 +0200

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,4 @@
-#!/usr/bin/nodejs
+#!/usr/bin/node
 
 function add(a, b) {
   return a + b;
@@ -8,7 +8,7 @@ const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 
 if (isNaN(arg1) || isNaN(arg2)) {
-  console.log("NaN");
+  console.log('NaN');
 } else {
   console.log(add(arg1, arg2));
 }
