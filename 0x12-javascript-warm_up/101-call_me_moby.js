71b38f170a348f045a57a02f723c0ec9033b0f58
Avumile <Smithavery435@gmail.com>
2024-02-14 08:25:52 -0800

    trial withsemestandarderror

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const executeXTimes = function(x, theFunction) {
-    for (let i = 0; i < x; i++) {
-        theFunction(); 
-    }
+const executeXTimes = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };
 module.exports = executeXTimes;
---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const addMeMaybe = function (number, theFunction) {
+  number++;
+  theFunction(number);
+};
+
+module.exports = {
+  addMeMaybe: addMeMaybe
+};
