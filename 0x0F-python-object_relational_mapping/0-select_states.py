d106cd858827bd756d0c24c8ea186439f29638cf
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-16 17:35:10 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
 """
-This script connects to a MySQL database, retrieves states from the 'states' table,
-and displays them sorted by ID. Arguments: username, password, database name.
+Connects to a MySQL database and retrieves all states from the table.
 """
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    try:
-        conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            charset="utf8"
-        )
-        
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
