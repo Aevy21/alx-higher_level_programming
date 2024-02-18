34b083dc93f1734e04088e3d76464875c278145c
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-18 02:23:23 +0200

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -26,7 +26,7 @@ def print_state_by_name():
         # Query for the State object with the specified name
         state = session.query(State).filter(State.name == sys.argv[4]).first()
 
-        # Print the ID of the State object if found, otherwise print "Not found"
+        
         if state:
             print(state.id)
         else:
@@ -38,7 +38,7 @@ def print_state_by_name():
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: ./script.py <username> <password> <database_name> <state_name>")
+        print(" ./script.py <username> <password> <database_name> <state_name>")
         sys.exit(1)
 
     print_state_by_name()
