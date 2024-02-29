b453319ab0b383f56e0c09c3901cb0aab4290fa0
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-29 23:43:24 +0200

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument and displays only the status code of the response.
-awk '/^HTTP/{if ($2 ~ /^[0-9]+$/) print $2}' < <(curl -sI "$1")
+curl -s -o /dev/null -w "%{http_code}" "$1"
