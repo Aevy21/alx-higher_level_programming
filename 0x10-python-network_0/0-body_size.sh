9c6ed55f7e466dd535eac593870ab6831051dea3
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-29 13:49:23 +0200

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as input, sends a request to that URL using curl in silent mode (-s), and displays the size of the body of the response in bytes.
-curl -s "$1" | wc -c
+curl -s "$1" | tail -c +$(($(curl -sI "$1" | awk '/Content-Length/{print $2}') + 1)) | wc -c
