d0c28975b64cc1ccb795464e2df00f9479e415e4
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-29 15:03:17 +0200

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as input, sends a GET request to the URL using curl, and displays the body of the response for a 200 status code.
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); if [ "$response" -eq 200 ]; then curl -s "$1"; fi
+response=$(curl -s -o /dev/null -w "%{http_code}" -X GET "$1"); if [ "$response" -eq 200 ]; then curl -s -X GET "$1"; fi
