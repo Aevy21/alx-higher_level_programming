95568211f62191a2244549d48476ddf3fab9835d
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-02 13:02:30 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,20 +4,15 @@
 This script fetches the status from https://alx-intranet.hbtn.io/status using urllib.
 """
 
+
 import urllib.request
+url = 'https://alx-intranet.hbtn.io/status'
 
-def fetch_status():
-    """
-    Fetches the status from https://alx-intranet.hbtn.io/status and prints it.
-    """
-    url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    utf8_content = body.decode('utf-8')
 
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-
-    print("- Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content.decode('utf-8'))
-
-if __name__ == "__main__":
-    fetch_status()
+print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", utf8_content)
