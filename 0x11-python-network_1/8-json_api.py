7d4e22b6d4b16e857b04e297f0cdc086e55b3d16
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-03 15:38:12 +0200

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-A script that takes a letter as input, sends a POST request to url with the letter as a parameter"""
+A script that takes a letter as input, sends a POST request to url with the letter as a parameter
+"""
 
 import requests
 import sys
@@ -14,7 +15,7 @@ if __name__ == "__main__":
 
     payload = {'q': q}
     url = 'http://0.0.0.0:5000/search_user'
-    
+
     response = requests.post(url, data=payload)
 
     try:
