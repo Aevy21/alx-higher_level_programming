ff3ff237b3a6bd82bb853fc20d08726454f27336
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-26 19:17:38 +0200

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,34 +2,13 @@
 
 const fs = require('fs');
 const request = require('request');
-
-// Check if both URL and file path arguments are provided
-if (process.argv.length < 4) {
-  console.error('Usage: ./fetch_and_store.js <url> <file_path>');
-  process.exit(1);
-}
-
-const url = process.argv[2];
+const apiURL = process.argv[2];
 const filePath = process.argv[3];
 
-// Make a GET request to the specified URL
-request.get({ url, encoding: 'utf-8' }, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching the webpage:', error);
-    return;
+request(apiURL, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFileSync(filePath, body);
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Error:', `Status Code ${response.statusCode}`);
-    return;
-  }
-
-  // Write the response body to the specified file path
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      return;
-    }
-    console.log(`Webpage content saved to ${filePath}`);
-  });
 });
